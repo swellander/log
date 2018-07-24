f70a7578040dbfb803dc0ddc63631ec35db30208
Sam Wellander <samwellander@gmail.com>
2018-07-24 15:59:59 -0400

    body parser and initial routes

--- index.js
+++ index.js
@@ -3,14 +3,30 @@ const app = express();
 const port = process.env.PORT || 3000;
 const client = require('./db');
 const TodoList = require('./todoList');
+const taskList = require('./views/taskList');
+const bodyParser = require('body-parser');
+const uuid = require('uuid');
 
 const list = new TodoList();
 list.add('Mow Lawn', 0);
 list.add('Pick up groceries', 1);
 list.add('Work out', 2);
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  console.log(req.method, req.url);
+  next();
+})
+
 app.get('/', (req, res) => {
-  res.send('This is the Home page!');
+  res.send(taskList(list.getTasks()));
+})
+
+app.post('/', (req, res) => {
+  const title = req.body.taskTitle;
+  list.add(title, uuid());
+  res.send(taskList(list.getTasks()));
 })
 
 app.listen(port, () => `Listening on port ${port}`);
