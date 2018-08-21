e9a5bdc18381d4ef5d752b5dae57ccdf469ecaff
Sam Wellander <samwellander@gmail.com>
2018-08-21 18:40:43 -0400

    add new task link to header

--- src/Header.js
+++ src/Header.js
@@ -3,9 +3,18 @@ import moment from 'moment';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
+  const plusStyles = {
+    color: 'blue'
+  }
+
   return (
     <nav className="nav-fill navbar navbar-expand-lg navbar-light bg-light">
-     <a className="navbar-brand" href="#">CodeLog</a>
+      <Link to="/"><a className="navbar-brand" href="#">CodeLog</a></Link>
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <Link to="/new"><i style={plusStyles} className="fas fa-plus"></i></Link>
+        </li>
+      </ul>
      <h3>{ moment().format('dddd, MMMM Do') }</h3>
     </nav> 
   )
