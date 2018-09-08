f2bc775351e61823e16a334e248ab149b5c32af8
Kevin Hu <pkvinhu@gmail.com>
2018-09-08 14:51:43 -0400

    calendar progress

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, {Component, Fragment} from 'react';
 
 
 
-export default class Calendar extends React.Component {
+export default class Calendar extends Component {
   constructor() {
     super();
     this.state = {
@@ -16,26 +16,43 @@ export default class Calendar extends React.Component {
   }
 
   render() {
-    let daysPerWk = 7
-    let totalDays = 31
-    let monthTable = []
-    let wk = []
-    while(totalDays >= 0) {
-      if(daysPerWk === 0 || totalDays === 0){
-        monthTable.push(wk)
-        wk = []
-        daysPerWk = 7
-      }
-      wk.push('')
-      daysPerWk--
-      totalDays--
+    function daysInThisMonth() {
+      var now = new Date();
+      return new Date(now.getFullYear(), now.getMonth()+1, 0).getDate();
     }
-    totalDays = 31
+    let totalDays = daysInThisMonth()
+    let count = 1;
+    console.log(totalDays)
+    let totalRows = 5
+    let monthTable = [['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']]
+    while(totalRows > 0) {
+      monthTable.push(Array(7).fill(count.toString()))
+      if (count<=totalDays) {count++}
+      else {count = ''}
+      totalRows--
+    }
+    
 
     return (
-      <div className="offset-4 col-md-6">
-        {monthTable.map(wk=>{
-
+      <div className="offset-1 col-md-1">
+        {monthTable.map((wk,colIdx)=>{
+          return(
+            <tr>
+            {wk.map(day=>{
+                return(
+                <Fragment>
+                  {colIdx===0 ? 
+                    <td style= {{ border: '1px solid black',
+                                  padding: '5px 60px 5px 60px'}}>{day}</td> :
+                    <td style= {{ border: '1px solid black',
+                                  padding:'50px 60px 50px 60px'}}>
+                                  <a style={{float: 'right'}}>{count}</a>
+                                  </td>}
+                </Fragment>
+                  )
+            })}
+          </tr>
+          )
         })}
       </div>
     )
